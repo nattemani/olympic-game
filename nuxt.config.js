9141fe89fa66b8b5bd38eb8e50f8e6422c48feae
nassim <nassim.attem@hotmail.com>
2023-03-10 05:24:51 +0100

    try to implement git pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  router: {
+    base: '/olympic-game/'
+  },
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
